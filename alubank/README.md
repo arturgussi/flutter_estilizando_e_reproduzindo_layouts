018f10a1daf75fa89504d9cf112e084b8a8d0557
Artur Gussi <arturgussi@gmail.com>
2023-09-11 16:37:44 -0300

    Aula 2: Estilizando Widgets

--- alubank/README.md
+++ alubank/README.md
@@ -4,11 +4,11 @@ A new Flutter project.
 
 ## Getting Started
 
-Como ler um layout no Figma e planejar como será a sua estrutura em código;
+Aplicar os Widgets de estilização como TextStyle e BoxDecoration para mostrar textos e decorar o Header (cabeçalho do app);
 
-O processo de priorização de conteúdo antes de estilização;
+Lidar com Widgets de organização como Row, Column e Padding para delimitar os espaçamentos e alinhamentos de Widgets exibidos na tela;
 
-O que são Widgets de conteúdo;
+Identificar as diferenças entre os Widgets Text e Text.rich para exibir textos e parágrafos, bem como utilizá-los;
 
-Widgets Flutter e Widgets Material.
+Implementar um gradiente de cores dentro do Header.
 
--- alubank/lib/components/sections/header.dart
+++ alubank/lib/components/sections/header.dart
@@ -7,14 +7,57 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: <Widget>[
-          Column(children: <Widget>[
-            Text('\$1000.00'),
-            Text('Balanço Disponível')
-          ]),
-          Icon(Icons.account_circle)
-        ],
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color.fromRGBO(103, 99, 234, 1.0),
+            Color.fromRGBO(155, 105, 254, 1.0),
+            Color.fromRGBO(195, 107, 255, 1.0),
+          ],
+        ),
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(10),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                // Text('\$1000.00',
+                //     style:
+                // TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                Text.rich(
+                  TextSpan(
+                    text: '\$',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: '1000.00',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Balanço Disponível',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            Icon(Icons.account_circle, size: 42)
+          ],
+        ),
       ),
     );
   }
